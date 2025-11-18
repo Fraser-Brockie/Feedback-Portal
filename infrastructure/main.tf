9a4ceaed0620e87c35fb7ccd635e923c922e574d
Fraser-Brockie <fraserbrockie@yahoo.co.uk>
2025-11-18 12:28:49 +0000

    amending zone

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,52 +1,24 @@
 provider "aws" {
-  region     = "eu-west-2"
+  region = "eu-west-2" # London
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0f29c8402f8cce65c" # Amazon Linux 2 in London
   instance_type = "t2.micro"
-  key_name      = "your-key-pair"
+  key_name      = "feedback-key"          # must match your AWS key pair
 
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
               yum install -y httpd
-              systemctl start httpd
               systemctl enable httpd
+              systemctl start httpd
               echo "Apache is running" > /var/www/html/index.html
             EOF
 
   tags = {
     Name = "FeedbackDemoInstance"
   }
-}
-
-resource "aws_s3_bucket" "logs" {
-  bucket = "feedback-deployment-logs"
-}
-
-resource "aws_s3_bucket_acl" "logs_acl" {
-  bucket = aws_s3_bucket.logs.id
-  acl    = "private"
-}
-
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2_s3_access_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_policy" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
 output "ec2_public_ip" {
